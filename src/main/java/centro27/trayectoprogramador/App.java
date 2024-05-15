657f14cc93829442f906f8a0fce7263a8c14cb52
Martín Alejandro Estrada Saavedra <martin996@hotmail.com.ar>
2024-05-14 21:16:07 -0300

    first commit

---
+++ src/main/java/centro27/trayectoprogramador/App.java
@@ -0,0 +1,13 @@
+package centro27.trayectoprogramador;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
